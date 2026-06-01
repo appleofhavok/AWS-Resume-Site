e9b160cf56e982ef0f36703e06e76bbe7347befc
Apolonia Dogoli <Apple@Apples-MacBook-Pro.local>
2026-06-01 14:47:18 -0400

    removed counter from pages except homepage, attempted to increment js counter

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-    var n = localStorage.getItem('on_load_counter');
-
-    if (n === null) {
+ var n = localStorage.getItem('on_load_counter');
+ if (n === null) {
         n = 0;
     }
-
+    
     n++;
+    
 
     localStorage.setItem("on_load_counter", n);
 
